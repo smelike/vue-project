672ee17ab2ecc09de64b7453b250b2b4d1b3e2e5
James <james@xqopen.com>
2016-10-29 16:02:41 +0800

    modify  vue.js index.html

--- src/main.js
+++ src/main.js
@@ -1,9 +1,32 @@
 import Vue from 'vue';
 import App from './App';
 
+
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import Hello from './components/Hello.vue';
+
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter();
+
+router.map({
+  '/hello': {
+    component: Hello,
+  }
+});
+
+router.redirect({
+  '*': '/hello',
+});
+
+router.start(App, '#app');
 /* eslint-disable no-new */
+/*
 new Vue({
   el: '#app',
   template: '<App/>',
   components: { App },
 });
+*/
